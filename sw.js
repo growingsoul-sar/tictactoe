562b7308bc37487c327da0effef1adb63cbb5894
Sark <growingstar225@gmail.com>
2026-02-24 14:39:23 +0530

    NEO_V5

--- sw.js
+++ sw.js
@@ -1,14 +1,39 @@
-const CACHE_NAME = 'neo-match-v1';
-const assets = ['/tictactoe/', '/tictactoe/index.html'];
+const CACHE_NAME = 'neo-match-v4'; // Change this number every time you update!
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
 
+// Install Event - Sets up the cache
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+    self.skipWaiting(); // Forces the new service worker to take over immediately
 });
 
+// Activate Event - Cleans up old versions of the cache
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim(); // Immediately starts controlling the page
+});
+
+// Fetch Event - Serves the app from cache, but updates if possible
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
